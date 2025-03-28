b83c4e8987e2b6a5c74fe4586b63c70575f608fb
ImMinSub <162276715+ImMinSub@users.noreply.github.com>
2025-03-28 17:11:29 +0900

    Update ApiService.kt

--- capstone/app/src/main/java/com/example/capstone/network/ApiService.kt
+++ capstone/app/src/main/java/com/example/capstone/network/ApiService.kt
@@ -1,1 +1,18 @@
+package com.example.capstone.network
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+// API 그룹 매칭 인터페이스
+interface ApiService {
+  @POST("find_group")
+  suspend fun findGroup( @Body request : FindGroupRequest ) : Response<FindGroupResponse>
+
+  @POST("login")
+  suspend fun login( @Body request : Login ) : Response<Login>
+
+  @POST("")
+}
+
 
