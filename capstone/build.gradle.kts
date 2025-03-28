ba04d21dafdd6225837458e2bd47706bb0e30fd9
ImMinSub <162276715+ImMinSub@users.noreply.github.com>
2025-03-28 15:46:26 +0900

    Update build.gradle.kts

--- capstone/build.gradle.kts
+++ capstone/build.gradle.kts
@@ -2,10 +2,10 @@
 plugins {
     id("com.android.application") version "8.9.0" apply false
     id("com.android.library") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.0.0" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
